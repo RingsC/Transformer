b3e662df952203bb15fab2482df5071ee4ef290c
root <homli@qiyi.com>
2017-07-17 15:11:25 +0800

    the rename lex output header file

---
+++ src/include/query/parser/lex/scannerGramm.h
@@ -0,0 +1,75 @@
+/*
+*	In this file,we define some functions used in parser.
+*
+*
+*	path:
+*	include/query/parser/lex/scanner.h
+*/
+#ifndef __SCANNER_H__
+#define __SCANNER_H__
+
+#include <cstddef>
+#include "types/types.h"
+#include "types/nodes.h"
+#include "keywords/keywords.h"
+
+#define YYLTYPE int 
+typedef void* Trans_yyscan_t ; 
+
+//saving the tokens.
+typedef union Trans_YYSTYPE	
+{
+	int 		ival_; 				//integer literals.
+	char*		str_; 				//string literals.
+	const char* keyword_;	//keywords.
+} Trans_YYSTYPE ;
+
+
+//for the scanner extra data. 
+typedef struct Trans_yy_extra_type {
+	char* 		scanbuffer_; 
+	size_t 		scanbuffer_len_;
+	
+	const struct Transformer::KeyWords::scan_keyword_t* \
+			 	keywords_;
+	int 		keywords_num_;
+	
+	int			backslash_quote_;
+	bool		escape_string_warning_;
+	bool		standard_conforming_strings_;
+
+	char *		literalbuffer_;		/* palloc'd expandable buffer */
+	int			literallen_;		/* actual current string length */
+	int			literalalloc_;	/* current allocated buffer size */	
+
+	
+	int			xcdepth_;		/* depth of nesting in slash-star comments */
+	char*		dolqstart_;		/* current $foo$ quote start string */
+
+
+	int32		utf16_first_part_;	
+
+	bool		warn_on_first_escape_;
+	bool		saw_non_ascii_;	
+
+} Trans_yy_extra_type ;
+
+
+typedef enum
+{
+	BACKSLASH_QUOTE_OFF,
+	BACKSLASH_QUOTE_ON,
+	BACKSLASH_QUOTE_SAFE_ENCODING
+}	BackslashQuoteType;
+
+/**************************************************************************************************************************/
+//for the scanner.
+extern Trans_yyscan_t  
+		init_scanner (const char* parseStr, Trans_yy_extra_type* yyext, const Transformer::KeyWords::ScanKeyWord keywords, int num_kw);
+extern void
+		finish_scanner(Trans_yyscan_t yyscanner); 		   
+
+extern int Trans_yylex(Trans_YYSTYPE *lvalp, YYLTYPE *llocp, Trans_yyscan_t yyscanner);
+extern int Trans_scanner_errposition(int location, Trans_yyscan_t yyscanner);
+
+#endif //__SCANNNER_H__ 
